5961c50c88fa4054a6257af5c6786f58a60d6581
vagrant <tommy@izettle.com>
2013-04-12 01:56:32 +0200

    Added timed output to specify which timed metrics to collect

--- src/collectors/dropwizard/dropwizard.py
+++ src/collectors/dropwizard/dropwizard.py
@@ -38,8 +38,9 @@ class DropwizardCollector(diamond.collector.Collector):
         config = super(DropwizardCollector, self).get_default_config()
         config.update({
             'host':     '127.0.0.1',
-            'port':     8081,
+            'port':     9091,
             'path':     'dropwizard',
+            'timed':    ''
         })
         return config
 
@@ -58,7 +59,7 @@ class DropwizardCollector(diamond.collector.Collector):
         try:
             result = json.load(response)
         except (TypeError, ValueError):
-            self.log.error("Unable to parse response from elasticsearch as a"
+            self.log.error("Unable to parse response from dropwizard as a"
                            + " json object")
             return
 
@@ -68,12 +69,26 @@ class DropwizardCollector(diamond.collector.Collector):
         mempool = memory['memory_pool_usages']
         jvm = result['jvm']
         thread_st = jvm['thread-states']
+        logback = result['ch.qos.logback.core.Appender']
+        jetty = result['org.eclipse.jetty.servlet.ServletContextHandler']
+
+        metrics['org.eclipse.jetty.servlet.ServletContextHandler.2xx-responses.1MinuteRate'] = jetty['2xx-responses']['m1']
+        metrics['org.eclipse.jetty.servlet.ServletContextHandler.4xx-responses.1MinuteRate'] = jetty['4xx-responses']['m1']
+        metrics['org.eclipse.jetty.servlet.ServletContextHandler.5xx-responses.1MinuteRate'] = jetty['5xx-responses']['m1']
+
+        metrics['ch.qos.logback.core.Appender.info.1MinuteRate'] = logback['info']['m1']
+        metrics['ch.qos.logback.core.Appender.warn.1MinuteRate'] = logback['warn']['m1']
+        metrics['ch.qos.logback.core.Appender.error.1MinuteRate'] = logback['error']['m1']
 
         metrics['jvm.memory.totalInit'] = memory['totalInit']
         metrics['jvm.memory.totalUsed'] = memory['totalUsed']
         metrics['jvm.memory.totalMax'] = memory['totalMax']
         metrics['jvm.memory.totalCommitted'] = memory['totalCommitted']
 
+        metrics['jvm.memory.totalInit'] = memory['totalInit']
+        metrics['jvm.memory.totalUsed'] = memory['totalUsed']
+        metrics['jvm.memory.totalMax'] = memory['totalMax']
+        metrics['jvm.memory.totalCommitted'] = memory['totalCommitted']
         metrics['jvm.memory.heapInit'] = memory['heapInit']
         metrics['jvm.memory.heapUsed'] = memory['heapUsed']
         metrics['jvm.memory.heapMax'] = memory['heapMax']
@@ -81,10 +96,18 @@ class DropwizardCollector(diamond.collector.Collector):
         metrics['jvm.memory.heap_usage'] = memory['heap_usage']
         metrics['jvm.memory.non_heap_usage'] = memory['non_heap_usage']
         metrics['jvm.memory.code_cache'] = mempool['Code Cache']
-        metrics['jvm.memory.eden_space'] = mempool['PS Eden Space']
-        metrics['jvm.memory.old_gen'] = mempool['PS Old Gen']
-        metrics['jvm.memory.perm_gen'] = mempool['PS Perm Gen']
-        metrics['jvm.memory.survivor_space'] = mempool['PS Survivor Space']
+        metrics['jvm.memory.eden_space'] = mempool['Eden Space']
+        metrics['jvm.memory.perm_gen'] = mempool['Perm Gen']
+        metrics['jvm.memory.survivor_space'] = mempool['Survivor Space']
+        metrics['jvm.daemon_thread_count'] = jvm['daemon_thread_count']
+        metrics['jvm.thread_count'] = jvm['thread_count']
+        metrics['jvm.fd_usage'] = jvm['fd_usage']
+        metrics['jvm.thread_states.timed_waiting'] = thread_st['timed_waiting']
+        metrics['jvm.thread_states.runnable'] = thread_st['runnable']
+        metrics['jvm.thread_states.blocked'] = thread_st['blocked']
+        metrics['jvm.thread_states.waiting'] = thread_st['waiting']
+        metrics['jvm.thread_states.new'] = thread_st['new']
+        metrics['jvm.thread_states.terminated'] = thread_st['terminated']
 
         metrics['jvm.daemon_thread_count'] = jvm['daemon_thread_count']
         metrics['jvm.thread_count'] = jvm['thread_count']
@@ -96,6 +119,14 @@ class DropwizardCollector(diamond.collector.Collector):
         metrics['jvm.thread_states.waiting'] = thread_st['waiting']
         metrics['jvm.thread_states.new'] = thread_st['new']
         metrics['jvm.thread_states.terminated'] = thread_st['terminated']
+
+	# Timed metrics
+	if self.config['timed']:
+		for stat in self.config['timed'].split(" "):
+			for resource in result[stat]:
+				metrics['.'.join([stat, resource, "duration", "mean"])] = result[stat][resource]['duration']['mean']
+				metrics['.'.join([stat, resource, "duration", "p98"])] = result[stat][resource]['duration']['p98']
+				metrics['.'.join([stat, resource, "rate", "1MinuteRate"])] = result[stat][resource]['rate']['m1']
 
         for key in metrics:
             self.publish(key, metrics[key])
