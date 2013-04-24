7cf7f0db06d78b21020bcd43cfe935cf6e18e4eb
vagrant <tommy@izettle.com>
2013-04-24 17:57:46 +0200

    Configurable metrics to collect

--- src/collectors/dropwizard/dropwizard.py
+++ src/collectors/dropwizard/dropwizard.py
@@ -40,12 +40,14 @@ class DropwizardCollector(diamond.collector.Collector):
         """
         config = super(DropwizardCollector, self).get_default_config()
         config.update({
-            'timed':    ''
             'host':	'127.0.0.1',
             'port':	9091,
             'path':	'dropwizard',
             'secure':	False,
             'url_path': 'metrics',
+	    'collect_jvm': True,
+	    'collect_jetty': True,
+            'collect_logback': True,
             'timed':    ''
         })
         return config
@@ -87,60 +89,68 @@ class DropwizardCollector(diamond.collector.Collector):
 
         metrics = {}
 
-        memory = result['jvm']['memory']
-        mempool = memory['memory_pool_usages']
-        jvm = result['jvm']
-        thread_st = jvm['thread-states']
-        logback = result['ch.qos.logback.core.Appender']
-        jetty = result['org.eclipse.jetty.servlet.ServletContextHandler']
+	# Jetty metrics	
+	if self.config['collect_jetty']:
+        	jetty = result['org.eclipse.jetty.servlet.ServletContextHandler']
+        	metrics['org.eclipse.jetty.servlet.ServletContextHandler.2xx-responses.1MinuteRate'] = jetty['2xx-responses']['m1']
+        	metrics['org.eclipse.jetty.servlet.ServletContextHandler.4xx-responses.1MinuteRate'] = jetty['4xx-responses']['m1']
+        	metrics['org.eclipse.jetty.servlet.ServletContextHandler.5xx-responses.1MinuteRate'] = jetty['5xx-responses']['m1']
 
-        metrics['org.eclipse.jetty.servlet.ServletContextHandler.2xx-responses.1MinuteRate'] = jetty['2xx-responses']['m1']
-        metrics['org.eclipse.jetty.servlet.ServletContextHandler.4xx-responses.1MinuteRate'] = jetty['4xx-responses']['m1']
-        metrics['org.eclipse.jetty.servlet.ServletContextHandler.5xx-responses.1MinuteRate'] = jetty['5xx-responses']['m1']
+	# Logback metrics	
+	if self.config['collect_logback']:
+        	logback = result['ch.qos.logback.core.Appender']
+        	metrics['ch.qos.logback.core.Appender.info.1MinuteRate'] = logback['info']['m1']
+        	metrics['ch.qos.logback.core.Appender.warn.1MinuteRate'] = logback['warn']['m1']
+        	metrics['ch.qos.logback.core.Appender.error.1MinuteRate'] = logback['error']['m1']
 
-        metrics['ch.qos.logback.core.Appender.info.1MinuteRate'] = logback['info']['m1']
-        metrics['ch.qos.logback.core.Appender.warn.1MinuteRate'] = logback['warn']['m1']
-        metrics['ch.qos.logback.core.Appender.error.1MinuteRate'] = logback['error']['m1']
+	# JVM metrics
+	if self.config['collect_jvm']:
+        	memory = result['jvm']['memory']
+        	mempool = memory['memory_pool_usages']
+        	jvm = result['jvm']
+        	thread_st = jvm['thread-states']
 
-        metrics['jvm.memory.totalInit'] = memory['totalInit']
-        metrics['jvm.memory.totalUsed'] = memory['totalUsed']
-        metrics['jvm.memory.totalMax'] = memory['totalMax']
-        metrics['jvm.memory.totalCommitted'] = memory['totalCommitted']
+        	metrics['jvm.memory.totalInit'] = memory['totalInit']
+        	metrics['jvm.memory.totalUsed'] = memory['totalUsed']
+        	metrics['jvm.memory.totalMax'] = memory['totalMax']
+        	metrics['jvm.memory.totalCommitted'] = memory['totalCommitted']
+        	metrics['jvm.memory.totalInit'] = memory['totalInit']
+        	metrics['jvm.memory.totalUsed'] = memory['totalUsed']
+        	metrics['jvm.memory.totalMax'] = memory['totalMax']
+        	metrics['jvm.memory.totalCommitted'] = memory['totalCommitted']
+        	metrics['jvm.memory.heapInit'] = memory['heapInit']
+        	metrics['jvm.memory.heapUsed'] = memory['heapUsed']
+        	metrics['jvm.memory.heapMax'] = memory['heapMax']
+        	metrics['jvm.memory.heapCommitted'] = memory['heapCommitted']
+        	metrics['jvm.memory.heap_usage'] = memory['heap_usage']
+        	metrics['jvm.memory.non_heap_usage'] = memory['non_heap_usage']
 
-        metrics['jvm.memory.totalInit'] = memory['totalInit']
-        metrics['jvm.memory.totalUsed'] = memory['totalUsed']
-        metrics['jvm.memory.totalMax'] = memory['totalMax']
-        metrics['jvm.memory.totalCommitted'] = memory['totalCommitted']
-        metrics['jvm.memory.heapInit'] = memory['heapInit']
-        metrics['jvm.memory.heapUsed'] = memory['heapUsed']
-        metrics['jvm.memory.heapMax'] = memory['heapMax']
-        metrics['jvm.memory.heapCommitted'] = memory['heapCommitted']
-        metrics['jvm.memory.heap_usage'] = memory['heap_usage']
-        metrics['jvm.memory.non_heap_usage'] = memory['non_heap_usage']
-        metrics['jvm.memory.code_cache'] = mempool['Code Cache']
-        metrics['jvm.memory.eden_space'] = mempool['Eden Space']
-        metrics['jvm.memory.perm_gen'] = mempool['Perm Gen']
-        metrics['jvm.memory.survivor_space'] = mempool['Survivor Space']
-        metrics['jvm.daemon_thread_count'] = jvm['daemon_thread_count']
-        metrics['jvm.thread_count'] = jvm['thread_count']
-        metrics['jvm.fd_usage'] = jvm['fd_usage']
-        metrics['jvm.thread_states.timed_waiting'] = thread_st['timed_waiting']
-        metrics['jvm.thread_states.runnable'] = thread_st['runnable']
-        metrics['jvm.thread_states.blocked'] = thread_st['blocked']
-        metrics['jvm.thread_states.waiting'] = thread_st['waiting']
-        metrics['jvm.thread_states.new'] = thread_st['new']
-        metrics['jvm.thread_states.terminated'] = thread_st['terminated']
+        	metrics['jvm.memory.code_cache'] = mempool['Code Cache']
+        	metrics['jvm.memory.eden_space'] = mempool['Eden Space']
+        	metrics['jvm.memory.perm_gen'] = mempool['Perm Gen']
+        	metrics['jvm.memory.survivor_space'] = mempool['Survivor Space']
 
-        metrics['jvm.daemon_thread_count'] = jvm['daemon_thread_count']
-        metrics['jvm.thread_count'] = jvm['thread_count']
-        metrics['jvm.fd_usage'] = jvm['fd_usage']
+        	metrics['jvm.daemon_thread_count'] = jvm['daemon_thread_count']
+        	metrics['jvm.thread_count'] = jvm['thread_count']
+        	metrics['jvm.fd_usage'] = jvm['fd_usage']
 
-        metrics['jvm.thread_states.timed_waiting'] = thread_st['timed_waiting']
-        metrics['jvm.thread_states.runnable'] = thread_st['runnable']
-        metrics['jvm.thread_states.blocked'] = thread_st['blocked']
-        metrics['jvm.thread_states.waiting'] = thread_st['waiting']
-        metrics['jvm.thread_states.new'] = thread_st['new']
-        metrics['jvm.thread_states.terminated'] = thread_st['terminated']
+        	metrics['jvm.thread_states.timed_waiting'] = thread_st['timed_waiting']
+        	metrics['jvm.thread_states.runnable'] = thread_st['runnable']
+        	metrics['jvm.thread_states.blocked'] = thread_st['blocked']
+        	metrics['jvm.thread_states.waiting'] = thread_st['waiting']
+        	metrics['jvm.thread_states.new'] = thread_st['new']
+        	metrics['jvm.thread_states.terminated'] = thread_st['terminated']
+
+        	metrics['jvm.daemon_thread_count'] = jvm['daemon_thread_count']
+        	metrics['jvm.thread_count'] = jvm['thread_count']
+        	metrics['jvm.fd_usage'] = jvm['fd_usage']
+
+        	metrics['jvm.thread_states.timed_waiting'] = thread_st['timed_waiting']
+        	metrics['jvm.thread_states.runnable'] = thread_st['runnable']
+        	metrics['jvm.thread_states.blocked'] = thread_st['blocked']
+        	metrics['jvm.thread_states.waiting'] = thread_st['waiting']
+        	metrics['jvm.thread_states.new'] = thread_st['new']
+        	metrics['jvm.thread_states.terminated'] = thread_st['terminated']
 
 	# Timed metrics
 	if self.config['timed']:
